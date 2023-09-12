97675805da13ad0e2dd4094dd7e822caa46cd72a
João Marcos Gusmão Teixeira <jmarkim.dev@outlook.com>
2023-09-12 16:16:03 -0300

    Implementa loop básico

--- Program.cs
+++ Program.cs
@@ -2,7 +2,27 @@
 using Op = ConsoleBlackjack.Operacoes;
 
 // Loop principal
+
+const string Comprar = "Comprar";
+const string Parar = "Parar";
+
+List<string> cartasJogador = new();
+List<string> cartasIA = new();
+
 do
 {
+	IEnumerable<string> baralho = new List<string>();
+
+	int acaoJogador;
+	do
+	{
+		cartasJogador.Add(Regras.ComprarCarta(ref baralho));
+
+		Regras.EscreverPilha(cartasJogador, "Jogador");
+
+		acaoJogador = Op.LerOpcao("Escolha uma ação:", Comprar, Parar);
+	} while (acaoJogador == 0);
+
+	Regras.EscreverPilha(cartasJogador, "Jogador");
 
 } while (Op.LerBool("Outra partida?"));
--- Regras.cs
+++ Regras.cs
@@ -3,6 +3,32 @@ public static class Regras
 {
 	public static IEnumerable<string> Baralho => Embaralha(Constantes.BaralhoPadrão);
 
+	public static string ComprarCarta(ref IEnumerable<string> baralho )
+	{
+		if (baralho is null || !baralho.Any())
+			baralho = Baralho;
+
+		var carta = baralho.First();
+
+		baralho = baralho.Take(1..);
+
+		return carta;
+	}
+
+	public static void EscreverPilha ( IEnumerable<string> pilha, string jogador)
+	{
+		Console.WriteLine($"Cartas de {jogador}");
+		foreach ( var item in pilha )
+		{
+			Console.WriteLine (item);
+		}
+	}
+
+	public static int ValorCarta (string valor )
+	{
+		return 0;
+	}
+
 	private static IEnumerable<T> Embaralha<T>(IEnumerable<T> lista )
 	{
 		List<T> embaralhado = new();
@@ -15,9 +41,9 @@ public static class Regras
 		{
 			int pos = Random.Shared.Next(0, items.Count);
 			embaralhado.Add(items[pos]);
+			items.RemoveAt(pos);
 		}
 
 		return embaralhado;
 	}
-
 }
