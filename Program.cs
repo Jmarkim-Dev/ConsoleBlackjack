a9588155428359f48240a5db735ae69a870ebb9d
João Marcos Gusmão Teixeira <jmarkim.dev@outlook.com>
2023-09-23 13:02:03 -0300

    Segundo jogador humano

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using ConsoleBlackjack;
 using Op = ConsoleBlackjack.Operacoes;
 
+const string NomeJogadorA = "Jogador A";
+const string NomeJogadorB = "Jogador B";
+
 const string Comprar = "Comprar";
 const string Parar = "Parar";
 
@@ -8,38 +11,68 @@ do
 {
 	IEnumerable<string> baralho = new List<string>();
 
-	List<string> cartasJogador = new();
-	List<string> cartasIA = new();
+	List<string> cartasJogadorA = new();
+	List<string> cartasJogadorB = new();
 
-	int acaoJogador;
+	cartasJogadorA.Add(Regras.ComprarCarta(ref baralho));
+	cartasJogadorB.Add(Regras.ComprarCarta(ref baralho));
+
+	int acaoJogadorA;
+	int acaoJogadorB;
 	do
 	{
 		Console.WriteLine();
-		cartasJogador.Add(Regras.ComprarCarta(ref baralho));
+		Regras.EscreverPilha(cartasJogadorA, NomeJogadorA);
 
-		Regras.EscreverPilha(cartasJogador, "Jogador");
+		Console.WriteLine();
+		Regras.EscreverPilha(cartasJogadorB, NomeJogadorB);
 
-		acaoJogador = Op.LerOpcao("Escolha uma ação:", Comprar, Parar);
-	} while (acaoJogador == 0);
+		Console.WriteLine();
+		acaoJogadorA = Op.LerOpcao($"{NomeJogadorA}, Escolha uma ação:", Comprar, Parar);
 
-	int pontuacaoJogador = 0;
-	foreach (var carta in cartasJogador)
+		Console.WriteLine();
+		acaoJogadorB = Op.LerOpcao($"{NomeJogadorB}, Escolha uma ação:", Comprar, Parar);
+
+		if (acaoJogadorA == 0)
+			cartasJogadorA.Add(Regras.ComprarCarta(ref baralho));
+
+		if(acaoJogadorB == 0)
+			cartasJogadorB.Add(Regras.ComprarCarta(ref baralho));
+	} while (acaoJogadorA == 0 || acaoJogadorB == 0);
+
+	int pontuacaoJogadorA = 0;
+	foreach (var carta in cartasJogadorA)
 	{
-		pontuacaoJogador += Regras.ValorCarta(carta);
+		pontuacaoJogadorA += Regras.ValorCarta(carta);
+	}
+
+	int pontuacaoJogadorB = 0;
+	foreach (var carta in cartasJogadorB)
+	{
+		pontuacaoJogadorB += Regras.ValorCarta(carta);
 	}
 
 	Console.WriteLine();
-	Regras.EscreverPilha(cartasJogador, "Jogador");
-	Console.WriteLine($"Pontuação total: {pontuacaoJogador} pontos");
+	Regras.EscreverPilha(cartasJogadorA, NomeJogadorA);
+	Console.WriteLine($"Pontuação total: {pontuacaoJogadorA} pontos");
+	
 	Console.WriteLine();
+	Regras.EscreverPilha(cartasJogadorB, NomeJogadorB);
+	Console.WriteLine($"Pontuação total: {pontuacaoJogadorB} pontos");
 
-	if (pontuacaoJogador <= 21)
-	{
-		Console.WriteLine("Você venceu!");
-	}
-	else
-	{
-		Console.WriteLine("Você perdeu, mais sorte da próxima vez.");
+	Console.WriteLine();
+	if (pontuacaoJogadorA > 21 && pontuacaoJogadorB > 21)
+		Console.WriteLine("Empate!");
+	else if (pontuacaoJogadorA > 21 && pontuacaoJogadorB <= 21)
+		Console.WriteLine("Venceddor: " + NomeJogadorB);
+	else if (pontuacaoJogadorA <= 21 && pontuacaoJogadorB > 21)
+		Console.WriteLine("Venceddor: " + NomeJogadorA);
+	else if (pontuacaoJogadorA == pontuacaoJogadorB)
+		Console.WriteLine("Empate!!");
+	else 
+	{ 
+		string venceddor = pontuacaoJogadorA > pontuacaoJogadorB ? NomeJogadorA : NomeJogadorB;
+		Console.WriteLine("Venceddor: " + venceddor);
 	}
 
 } while (Op.LerBool("Outra partida?"));
